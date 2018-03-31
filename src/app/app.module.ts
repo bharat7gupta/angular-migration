087dc7cbee997a834b31b5a087336b49b9c4c97b
Bharat <bharat7gupta@gmail.com>
2018-03-31 08:54:28 +0530

    included angularjs app. made changes to app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { UpgradeModule } from '@angular/upgrade/static';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ imports: [
+   BrowserModule,
+   UpgradeModule
+ ]
 })
-export class AppModule { }
+export class AppModule {
+ constructor(private upgrade: UpgradeModule) { }
+ 
+ ngDoBootstrap() {
+   this.upgrade.bootstrap(document.body, ['demo'], { strictDi: true });
+ }
+}
